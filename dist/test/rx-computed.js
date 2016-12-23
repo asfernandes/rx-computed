82e5fe39bf6e8de4af2e33f8836accab8474fca0
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2016-12-23 12:11:20 -0200

    Update dist.

--- dist/test/rx-computed.js
+++ dist/test/rx-computed.js
@@ -34,10 +34,12 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var sourceMapSupport = require("source-map-support");
 var BehaviorSubject_1 = require("rxjs/BehaviorSubject");
 require("rxjs/Rx");
 var rx_computed_1 = require("../lib/rx-computed");
 var assert = require("power-assert");
+sourceMapSupport.install();
 describe("RxComputed", function () {
     var _this = this;
     it('sync without dependencies', function () {
